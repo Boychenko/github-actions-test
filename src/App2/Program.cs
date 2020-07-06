4a8093c10b555f0fb74a3e4d9fb3318981eb07d2
Alexander Boychenko <boichenko.alexander@gmail.com>
2020-07-06 15:13:47 +0700

    app2 update

--- src/App2/Program.cs
+++ src/App2/Program.cs
@@ -6,7 +6,7 @@ namespace App2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello4 World!");
+            Console.WriteLine("Hello5 World!");
         }
     }
 }
