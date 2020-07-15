3b616524f82de2170a27a57b5df5e2499eeaf1a4
Alexander Boychenko <boichenko.alexander@gmail.com>
2020-07-16 01:52:35 +0700

    tests

---
+++ src/App1.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace App1.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(0, new Random().Next(3, 100));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(0, 0);
+        }
+    }
+}
